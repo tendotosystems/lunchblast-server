a2dc2dd1999fa511dd48980288ad2c2231a478bd
pickledyamsman <pickledyamsman@gmail.com>
2017-06-01 16:12:14 -0400

    Adds guest user that isn't working correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+  before_action :authenticate_guest_or_user
 
-  before_action :authenticate_user
+  def authenticate_guest_or_user
+  	if current_user
+  	  authenticate_user
+  	else
+  	  current_user = User.find(1)
+  	  current_user.authenticate(token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTY0Mjg0MTYsInN1YiI6MSwiZW1haWwiOiJndWVzdEBibGFzdGFwcC5pbyJ9.0K345zG5RR8h3L0Qsv6d6mYj0qNIVWI4n2x1oTTfJQw')
+  	end
+  end
+
 end
