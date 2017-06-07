ceff4d0c3eff779ef3521eea7ff5def9b18a53ad
pickledyamsman <mattcianciolo@blastapp.io>
2017-06-07 15:06:14 -0400

    Moves jwt out of user serializer

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      render json: { jwt: Knock::AuthToken.new(payload: { sub: @user.id }).token, user: UserSerializer.new(@user) }, status: :created
     else
       render json: { errors: @user.errors }, status: :unprocessable_entity
     end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :jwt
-
-  def jwt
-  	Knock::AuthToken.new(payload: { sub: object.id }).token
-  end
-
+  attributes :id, :name, :email
 end
